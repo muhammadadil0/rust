a9626600070d12bb601d3205b4e0e00f45466bd4
Muhammad Adil <139769172+muhammadadil0@users.noreply.github.com>
2026-02-11 19:23:50 +0500

    Update struct_enum_impl.rs

--- struct_enum_impl.rs
+++ struct_enum_impl.rs
@@ -35,3 +35,69 @@ fn main(){
   
   user.show_status();
 }
+
+
+____________________________________________________- Struct _____________________________________________________
+
+
+
+
+struct Car{
+  
+  owner:String,
+  year:u32,
+  price:u32,
+}
+
+impl Car{
+  
+  fn display(&self){
+    println!("Owner is : {} Year is : {} Price is : {}",self.owner, self.year, self.price);
+  }
+  
+  
+  fn change_Price()->u32{
+    40_000
+    
+  }
+  
+  fn new_price (&self)->u32{
+    self.price + Self::change_Price()
+  }
+
+  
+   fn insert(owner:String,year:u32,price:u32)->Self {
+    
+    Self{
+      owner:owner,
+      year:year,
+      price:price
+    }
+  }
+}
+
+
+ 
+
+
+fn main(){
+  
+  let my_car = Car {
+    owner:String::from("Adil"),
+    year:2021,
+    price:30_000
+    
+  };
+  
+  
+  my_car.display();
+  let new_price = my_car.new_price();
+  println!("New price is : {}",new_price);
+  
+  let new_owner = Car::insert(String::from("Adnan"),2023,30_000);
+  
+  
+  println!("New owner is : {}", new_owner.owner);
+  
+}
+
